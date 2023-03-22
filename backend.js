096205f2fb622eba361510499fdb067392df3ad5
Rishabh Agrawal <89372462+rishu0087@users.noreply.github.com>
2023-03-22 18:10:08 +0530

    changes made

--- backend.js
+++ backend.js
@@ -1,4 +1,15 @@
 function myFunction() {
     var x = document.getElementById("url").value;
-    document.getElementById("display").innerHTML = x;
-  }
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+    // var url = 'www.geeksforgeeks.org';
+    // el_up.innerHTML = "URL = '" + url + "'";
+    var res = "";
+    if (x.match(regex)) {
+        res = "Valid URL";
+    } else {
+        res = "Invalid URL";
+    }
+    // el_down.innerHTML = res;
+    document.getElementById("display").innerHTML = res;
+}
