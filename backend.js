39ff93a4036b792a793f1d1ba2b5a82d98c71c89
Mudit Jain <1muditjain1@gmail.com>
2023-03-23 06:59:35 +0530

    changes in js

--- backend.js
+++ backend.js
@@ -1,4 +1,50 @@
 function myFunction() {
     var x = document.getElementById("url").value;
     document.getElementById("display").innerHTML = x;
+
+
+
+
+
+
+
+
+//     const { spawn } = require('child_process');
+// const url = 'https://example.com';
+
+// const pythonProcess = spawn('python', ['path/to/python/file.py', url]);
+
+// pythonProcess.stdout.on('data', (data) => {
+//   console.log(`Python output: ${data}`);
+// });
+
+// pythonProcess.stderr.on('data', (data) => {
+//   console.error(`Python error: ${data}`);
+// });
+
+// pythonProcess.on('close', (code) => {
+//   console.log(`Python process exited with code ${code}`);
+// });
+
+
+
+
+
+
+// const axios = require('axios');
+// const url = 'https://example.com';
+
+// axios.post('http://localhost:5000/api', {
+//   url: url
+// })
+// .then(response => {
+//   console.log(`Python output: ${response.data}`);
+// })
+// .catch(error => {
+//   console.error(`Python error: ${error}`);
+// });
+
+
+
+
   }
